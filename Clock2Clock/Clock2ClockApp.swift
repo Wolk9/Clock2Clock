b811fde4f844bfe7d7c0b1a3d4a4fdd4a6979551
Martin op iMac <wolk9@me.com>
2023-08-27 22:41:43 +0200

    Initial Commit

---
+++ Clock2Clock/Clock2ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock2ClockApp.swift
+//  Clock2Clock
+//
+//  Created by Macbook  on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Clock2ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
